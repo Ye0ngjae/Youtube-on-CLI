8001d93c2b1321a1c4cf4a7196a7fad7b2848ddd
Ye0ngjae <op@ye0ngjae.com>
2024-08-23 23:20:28 +0900

    Add Audio

--- source/main.py
+++ source/main.py
@@ -1,14 +1,24 @@
 from youtube_downloader import video_download
-from video_to_ascii import frame_from_video
+from video_to_ascii import frame_from_video, print_video
+import pyglet
 import os
 
 def main():
     url = input("Enter the URL of the YouTube video: ")
+
+    os.system('cls')
     video_name = video_download(url)
-    frame_from_video(video_name+'.mp4')
+
+    video = frame_from_video(video_name)
+
+    if video:
+        song = pyglet.media.load(f'audios/{video_name}.mp3')
+        song.play()
+        print_video(video)
 
     os.remove(f'audios/{video_name}.mp3')
     os.remove(f'videos/{video_name}.mp4')
+    os.system('cls')
 
 if __name__ == "__main__":
     main()
--- source/video_to_ascii.py
+++ source/video_to_ascii.py
@@ -5,13 +5,14 @@ import time
 CHARSET = " .:-=+*#%@"
 
 def frame_to_ascii(frame):
+    video = []
+
     height, width, _ = frame.shape
     aspect_ratio = width / height
-    new_width = 100  # Desired width of the ASCII art
+    new_width = 190  # Desired width of the ASCII art
     new_height = int(new_width / aspect_ratio / 2)  # Adjust height to maintain aspect ratio
     resized_frame = cv2.resize(frame, (new_width, new_height))
     
-
     # Convert the frame to grayscale
     gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
 
@@ -24,13 +25,14 @@ def frame_to_ascii(frame):
             ascii_index = int(gray_value / 255 * (len(CHARSET) - 1))
             ascii_frame += CHARSET[ascii_index]
         ascii_frame += "\n"
-    
-    os.system("clear")
-    print(ascii_frame) # Print the ASCII frame
-    time.sleep(1 / 33)  # Delay to match the video frame rate
+
+    video.append(ascii_frame)
+
+    return video
 
 def frame_from_video(video_name):
-    cap = cv2.VideoCapture('./videos/' + video_name)
+    cap = cv2.VideoCapture('./videos/' + video_name + '.mp4')
+    video = []  # Initialize the video array here
     
     while cap.isOpened():
         ret, frame = cap.read()
@@ -38,6 +40,15 @@ def frame_from_video(video_name):
         if not ret:
             break
         
-        frame_to_ascii(frame)
+        ascii_frame = frame_to_ascii(frame)
+        video.append(ascii_frame[0])  # Append the ASCII frame to the video list
         
     cap.release()
+    return video  # Return the video list
+
+def print_video(video):
+
+    for frame in video:
+        print("\033[0;0H")  # Print the ASCII frame
+        print(frame)
+        time.sleep(1/25)
